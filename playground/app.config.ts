427ebd05c7bfe39cb0566f1c93cce493f8484be4
ziga <zvidic@sleuth.io>
2024-08-01 10:12:24 +0200

    Minimize the diff

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -2,6 +2,11 @@ import { defineAppConfig } from '#imports'
 
 export default defineAppConfig({
   icon: {
-    mode: 'svg',
+    aliases: {
+      github: 'carbon:logo-github',
+      nuxt: 'logos:nuxt-icon',
+      rocket: 'skill-icons:rocket',
+      nxt: 'NuxtLogo',
+    },
   },
 })
--- playground/app.vue
+++ playground/app.vue
@@ -1,34 +1,18 @@
 <template>
-  <div class="flex flex-col gap-4">
-    <div class="border border-solid border-gray/10 p4 rounded-lg mt-20">
-      <p>
-        Custom icons from local fs:
-        <Icon
-          name="custom1:nuxt-v1"
-          size="64"
-        />
-        <Icon
-          name="custom1:nuxt-v2"
-          size="64"
-        />
-        <Icon
-          name="custom1:nuxt-v3-beta"
-          size="64"
-        />
-        <Icon
-          name="custom1:nuxt-v3"
-          size="64"
-        />
+  <Icon
+    name="custom1:nuxt-v3"
+    size="64"
+    mode="svg"
+  />
 
-        <!-- Workaround, load at least single icon in css mode to make svg work when set via app.config.ts -->
-        <!-- comment this one out and all above disappear -->
-        <Icon
-          style="display: none;"
-          name="custom1:nuxt-v3"
-          size="64"
-          mode="css"
-        />
-      </p>
-    </div>
-  </div>
+  <!--
+    WORKAROUND: load at least one icon in css mode to make svg mode work.
+    It doesn't even have to be the same icon
+  -->
+  <Icon
+    style="display: none;"
+    name="custom1:nuxt-v1"
+    size="64"
+    mode="css"
+  />
 </template>
